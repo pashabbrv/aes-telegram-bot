d888688d5debba963b46a306956cd28122b06614
Egor Miroevsky <57674715+k0rdun@users.noreply.github.com>
2025-03-11 21:05:46 +0300

    integrate email handler to bot

--- bot/email_handler.py
+++ bot/email_handler.py
@@ -1,12 +1,20 @@
+from dotenv import load_dotenv
+import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+# Загрузка констант из .env
+load_dotenv()
+from_email = os.getenv('SENDER_EMAIL')
+password = os.getenv('PASSWORD_EMAIL')
 
-def send_email(to_email: str, subject: str, body: str,
-               from_email: str, password: str,
-               smtp_server: str = "smtp.gmail.com",
-               smtp_port: int = 587) -> None:
+# Параметры SMTP сервера (Google)
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+
+
+def send_email(to_email: str, subject: str, body: str) -> None:
     """
     Отправляет электронное письмо через SMTP-сервер.
 
@@ -14,39 +22,37 @@ def send_email(to_email: str, subject: str, body: str,
     - to_email: Почта получателя.
     - subject: Тема письма.
     - body: Содержимое письма (может быть HTML или обычным текстом).
-    - from_email: Почта отправителя (должна поддерживать SMTP).
-    - password: Пароль от почты отправителя или пароль приложения.
-    - smtp_server: Адрес SMTP-сервера (по умолчанию для Gmail).
-    - smtp_port: Порт SMTP-сервера (по умолчанию для Gmail с TLS).
     """
+    global smtp_server
+    global smtp_port
 
-    # Создаем объект сообщения
+    # Создание объекта сообщения
     msg = MIMEMultipart()
     msg["From"] = from_email
     msg["To"] = to_email
     msg["Subject"] = subject
 
-    # Добавляем тело письма
-    msg.attach(MIMEText(body, "plain"))  # Для HTML-писем замените "plain" на "html"
+    # Добавление тела письма
+    msg.attach(MIMEText(body, "plain"))
 
     try:
         # Подключаемся к SMTP-серверу
         with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.starttls()  # Шифруем соединение
+            server.starttls()  # Шифрование соединения
             server.login(from_email, password)
             server.sendmail(from_email, to_email, msg.as_string())
         print("Письмо успешно отправлено!")
+        return True
 
     except Exception as e:
         print(f"Ошибка при отправке письма: {e}")
+        return False
 
 
 # Пример использования
 if __name__ == "__main__":
     send_email(
-        to_email="recipient@example.com",
+        to_email="your_mail@mail.ru",
         subject="Тестовое письмо",
         body="Привет, это тестовое письмо из Python!",
-        from_email="your_email@gmail.com",
-        password="your_password"
     )
