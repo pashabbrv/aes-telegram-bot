bf1ca825467c429ca4b388e6a49daf38a6d3683d
Pavel Bobrov <112647982+pashabbrv@users.noreply.github.com>
2025-03-24 04:28:26 +0300

    fix: update llm_integration.py imports

--- bot/llm_integration.py
+++ bot/llm_integration.py
@@ -11,7 +11,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from dotenv import load_dotenv
-from text_information import STOP_WORDS
+from .text_information import STOP_WORDS
 
 # Загрузка переменных окружения
 load_dotenv()
