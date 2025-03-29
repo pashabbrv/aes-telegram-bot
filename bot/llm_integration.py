35dec2aa0ffd62a389debb0d9e6ac991c789b7b8
Pavel Bobrov <112647982+pashabbrv@users.noreply.github.com>
2025-03-29 04:26:54 +0300

    feat: remove fallback answer, change yandex gpt to llama

--- bot/llm_integration.py
+++ bot/llm_integration.py
@@ -1,17 +1,13 @@
 import os
 import boto3
-import string
-from langchain_community.llms import YandexGPT
+import time
+from yandex_cloud_ml_sdk import YCloudML
 from langchain_community.embeddings.yandex import YandexGPTEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from qdrant_client import QdrantClient
 from qdrant_client.models import PointStruct
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough
 from dotenv import load_dotenv
-from .text_information import STOP_WORDS
 
 # Загрузка переменных окружения
 load_dotenv()
@@ -20,13 +16,22 @@ load_dotenv()
 qdrant_url = os.getenv("QDRANT_URL")
 qdrant_api_key = os.getenv("QDRANT_API_KEY")
 
-# Инициализация YandexGPT и эмбеддингов
+# Инициализация YandexCloud параметров и эмбеддингов YandexGPT
 api_key = os.getenv("YANDEX_API_KEY")
 folder_id = os.getenv("YANDEX_FOLDER_ID")
 embeddings = YandexGPTEmbeddings(api_key=api_key, folder_id=folder_id)
-yandex_gpt = YandexGPT(api_key=api_key, folder_id=folder_id)
 
-# Конфигурация S3
+# Инициализация Llama из YandexCloud
+sdk = YCloudML(
+        folder_id=folder_id,
+        auth=api_key,
+    )
+llama_model = sdk.models.completions("llama").configure(
+    temperature=0.5,
+    max_tokens=500
+)
+
+# Конфигурация S3 (Yandex Bucket)
 aws_access_key_id = os.getenv("AWS_ACCESS_KEY")
 aws_secret_access_key = os.getenv("AWS_SECRET_KEY")
 bucket_name = os.getenv("YANDEX_BUCKET_NAME")
@@ -82,78 +87,62 @@ def load_or_create_qdrant(bucket_name):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     texts = text_splitter.split_documents(documents)
     
-    points = [
-        PointStruct(id=i, vector=embeddings.embed_documents([text.page_content])[0], payload={"text": text.page_content})
-        for i, text in enumerate(texts)
-    ]
+    points = []
+    for i, text in enumerate(texts):
+        time.sleep(0.1)
+        
+        point = PointStruct(
+            id=i,
+            vector=embeddings.embed_documents([text.page_content])[0],
+            payload={"text": text.page_content}
+        )
+        points.append(point)
     
     qdrant_client.upsert(collection_name=collection_name, points=points)
-
-def extract_key_terms(question):
-    """Извлечь ключевые термины из вопроса, убирая стоп-слова."""
-    stop_words = STOP_WORDS
-    question = question.lower().translate(str.maketrans("", "", string.punctuation))
-    terms = [word for word in question.split() if word not in stop_words]
-    return terms if terms else question.split()
 
 def clean_text(text):
     """Удаляет спецсимволы и приводит текст к читаемому виду."""
     return text.replace("\n", " ").replace("\\n", " ").replace('"', '').strip()
 
-def contains_key_term(text, key_terms):
-    """Проверяет, содержит ли текст хотя бы одно из ключевых слов."""
-    text = text.lower()
-    return any(term in text for term in key_terms)
-
 def LLM_chain(question):
     """Основная функция для обработки запроса и возвращения ответа."""
     load_or_create_qdrant(bucket_name)
+    info = "\n\nЕсли не нашли ответ на свой вопрос, воспользуйтесь сайтом: https://pish.etu.ru/. Или задайте вопрос менеджеру программы."
     
     query_vector = embeddings.embed_documents([question])[0]
+    
     search_result = qdrant_client.query_points(
         collection_name=collection_name,
         query=query_vector,
-        limit=1000
+        limit=10
     ).points
-    
-    question_words = set(extract_key_terms(question))
-    relevant_results = [r for r in search_result if any(word in r.payload["text"].lower() for word in question_words)]
-    
-    if relevant_results and any(contains_key_term(r.payload["text"], question_words) for r in relevant_results):
-        context = "\n".join([clean_text(result.payload["text"]) for result in relevant_results[:10]])
-        
-        template = """Ты ассистент по вопросам, связанным с ПИШ и ЛЭТИ. Если в вопросе упоминается "ПИШ" или "ЛЭТИ", отвечай в их контексте. 
-        Если в вопросе нет явного указания на ПИШ или ЛЭТИ, подразумевай, что он о ПИШ. 
-        Используй предоставленный контекст для ответа. Если контекста недостаточно, дай общий ответ, связанный с ПИШ. Давай развернутый ответ.
+
+    if search_result:
+        context = "\n".join([clean_text(result.payload["text"]) for result in search_result])
+        #print(f"Контекст для запроса: {context[:300]}...")
+
+        prompt = """Ты ассистент по вопросам, связанным с ПИШ(передовой инженерной школой) ЛЭТИ. Отвечай в контексте ПИШ ЛЭТИ.
+        Не отвечай на вопросы про политику, противозаконные действия, порнографию, войну и другие провокационные вещи.
+        Используй предоставленный контекст для ответа. Не упоминай контекст в ответе. Дай ответ максимум в 450 токенов.
         Контекст: {context}
         Вопрос: {question}
         """
-        
-        prompt = ChatPromptTemplate.from_template(template)
-        chain = (
-            {"context": RunnablePassthrough(), "question": RunnablePassthrough()}
-            | prompt
-            | yandex_gpt
-            | StrOutputParser()
-        )
-        
-        response = chain.invoke({"context": context, "question": question})
-    else:
-        response = fallback_response(question)
-    
-    return response + "\n\nЕсли не нашли ответ на свой вопрос, воспользуйтесь сайтом: https://pish.etu.ru/. Или задайте вопрос менеджеру программы."
+        prompt = prompt.format(context=context, question=question)
+        #print(template)
 
-def fallback_response(question):
-    template = """Ты универсальный GPT ассистент давай развернутые ответы на вопросы.
-    Вопрос: {question}
-    """
-    prompt = ChatPromptTemplate.from_template(template)
-    chain = prompt | yandex_gpt | StrOutputParser()
-    return chain.invoke({"question": question})
+        result = llama_model.run(prompt)
+        response = result.alternatives[0].text
+
+        if response == "В интернете есть много сайтов с информацией на эту тему. [Посмотрите, что нашлось в поиске](https://ya.ru)":
+            return "Не могу ответить на данный вопрос. Скорее всего он не связан с ПИШ ЛЭТИ." + info
+    else:
+        return "Не получилось ничего найти по данному запросу." + info
+    
+    return response + info
 
 if __name__ == "__main__":
     # Тестовые вопросы
-    questions = ["промышленная электроника", "напиши легкую html страницу"]
+    questions = ["война на украине"]
     for question in questions:
         print(f"\nВопрос: {question}")
         answer = LLM_chain(question)
