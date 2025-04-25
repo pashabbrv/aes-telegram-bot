682f0471cf29988c3ce65ea6b4587b40bdca5c08
Egor Miroevsky <egor932005@mail.ru>
2025-04-25 18:12:00 +0300

    fix: edit text in statistic module

--- bot/bot_handlers/bot_statistic.py
+++ bot/bot_handlers/bot_statistic.py
@@ -3,7 +3,7 @@ import redis
 import os
 
 from .bot_states import MainMenuState
-from ..managers import managers, management
+from ..managers import management
 
 # Подключение к redis
 redis_host = os.getenv('REDIS_HOST')
@@ -23,14 +23,14 @@ def register_commands(bot: TeleBot):
         func=lambda msg: msg.text == 'Статистика' and msg.from_user.id in management,
     )
     def main_handler(message):
-        managers_count = 0
-        for value in set(managers.values()):
+        management_count = 0
+        for value in management:
             if value != 0:
-                managers_count += 1
+                management_count += 1
         all_users_count = redis_storage.dbsize()
         bot.send_message(
             chat_id=message.chat.id,
-            text=f'- Количество назначенных менеджеров: {managers_count}\n'
-            f'- Количество пользователей (без менеджеров): {max(0, all_users_count - managers_count)}\n'
+            text=f'- Дирекция, имеющая доступ к статистике: {management_count}\n'
+            f'- Количество пользователей (без дирекции): {max(0, all_users_count - management_count)}\n'
         )
     
